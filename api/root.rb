4efd27fd52bfb6cf4f9b72beae72061fd4d7aece
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-07-23 10:31:05 +0200

    More API description

--- api/root.rb
+++ api/root.rb
@@ -31,7 +31,7 @@ module Api
 
     documentation_class = add_swagger_documentation base_path: (lambda do |request| "#{request.scheme}://#{request.host}:#{request.port}" end), hide_documentation_path: true, info: {
       title: ::AddokWrapper::config[:product_title],
-      description: 'API access require an api_key.',
+      description: 'API access require an api_key. API results are geojson extended by geocodejson-spec on version draft#namespace#score.',
       contact: ::AddokWrapper::config[:product_contact]
     }
 
