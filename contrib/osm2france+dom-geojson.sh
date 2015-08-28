c3f36fbae9331ef3721c284a801d5ff5e57df7f4
Fabien Girard <fabien@mapotempo.com>
2015-08-28 16:38:02 +0200

    Correct shell to download geojson

--- contrib/osm2france+dom-geojson.sh
+++ contrib/osm2france+dom-geojson.sh
@@ -2,11 +2,11 @@
 
 # Download French boundaries
 IDS="1403916 1401835 1891495 1260551 1785276 1259885"
-#rm *.geojson
-#for ID in $IDS ; do
-#  wget "http://polygons.openstreetmap.fr/?id=$ID&params=0" -O /dev/null # Call the generator
-#  wget "http://polygons.openstreetmap.fr/get_geojson.py?id=$ID&params=0" -O $ID.geojson
-#done
+rm *.geojson
+for ID in $IDS ; do
+ wget "http://polygons.openstreetmap.fr/?id=$ID&params=0" -O /dev/null # Call the generator
+ wget "http://polygons.openstreetmap.fr/get_geojson.py?id=$ID&params=0" -O $ID.geojson
+done
 
 # Merge geojson
 cat 1*.geojson | \
