753e2664906ec89b6b19331ebd969021b5578a93
Alexis Lahouze <alexis@lahouze.org>
2017-06-28 15:51:22 +0200

    Fix initialization commands.

--- docker/initialize.sh
+++ docker/initialize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget https://bano.openstreetmap.fr/BAN_odbl/BAN_odbl.json.bz2 -O data/BAN_odbl.json.bz2
-bzcat data/BAN_odbl.json.bz2 | docker-compose -p geocoder exec -T addok addok --config /etc/addok/config.py batch
+docker-compose -p geocoder run --volume $PWD/data:/data --entrypoint /bin/bash addok -c "bzcat data/BAN_odbl.json.bz2 | addok --config /etc/addok/config.py batch"
 
-ls data/*.json | xargs  cat | docker-compose -p geocoder exec -T addok addok --config /etc/addok/config.py batch
+docker-compose -p geocoder run --volume $PWD/data:/data --entrypoint /bin/bash addok -c "ls data/*.json | xargs cat | addok --config /etc/addok/config.py batch"
 
 docker-compose -p geocoder exec addok addok --config /etc/addok/config.py ngrams
 
