e800011f732d4b63f2ac7e471fe0a4ff9c457c95
Alexis Lahouze <xals@lahouze.org>
2017-07-03 11:17:11 +0200

    Fix config file for addok.

--- docker/initialize.sh
+++ docker/initialize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget https://bano.openstreetmap.fr/BAN_odbl/BAN_odbl.json.bz2 -O data/BAN_odbl.json.bz2
-docker-compose -p geocoder run --volume $PWD/data:/data --entrypoint /bin/bash addok -c "bzcat data/BAN_odbl.json.bz2 | addok --config /etc/addok/config.py batch"
+docker-compose -p geocoder run --volume $PWD/data:/data --entrypoint /bin/bash addok -c "bzcat data/BAN_odbl.json.bz2 | addok batch"
 
-docker-compose -p geocoder run --volume $PWD/data:/data --entrypoint /bin/bash addok -c "ls data/*.json | xargs cat | addok --config /etc/addok/config.py batch"
+docker-compose -p geocoder run --volume $PWD/data:/data --entrypoint /bin/bash addok -c "ls data/*.json | xargs cat | addok batch"
 
-docker-compose -p geocoder exec addok addok --config /etc/addok/config.py ngrams
+docker-compose -p geocoder exec addok addok ngrams
 
