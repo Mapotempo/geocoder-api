630638649bc380e08a9c301fa1db309af91365c9
Fabien Girard <fabien@mapotempo.com>
2018-07-05 15:23:21 +0200

    Add comment

--- docker/initialize-fr.sh
+++ docker/initialize-fr.sh
@@ -15,11 +15,15 @@ docker-compose -p ${PROJECT} rm -sf redis-server-fr
 rm -f data-fr/*
 docker-compose -p ${PROJECT} up -d redis-server-fr
 
+# Download and load BAN
 wget https://bano.openstreetmap.fr/BAN_odbl/BAN_odbl.json.bz2 -O addresses-fr/BAN_odbl.json.bz2
 docker-compose -p ${PROJECT} run --entrypoint /bin/bash addok-fr -c "bzcat addresses/BAN_odbl.json.bz2 | jq -c 'del(.housenumbers[]?.id)' | addok batch"
 
+# Patch BAN
 docker-compose -p ${PROJECT} run --entrypoint /bin/bash addok-fr -c "ls addresses/*.json | xargs cat | addok batch"
 
+# Index
 docker-compose -p ${PROJECT} exec addok-fr addok ngrams
 
+# Save redis db into file
 docker-compose -p ${PROJECT} exec redis-server-fr redis-cli BGSAVE
