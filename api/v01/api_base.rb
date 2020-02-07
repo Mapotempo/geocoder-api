60bf98e91b7f65e50bd851a2ed00b69034b67091
yann-g <yann.gregoire@mapotempo.com>
2020-02-07 19:51:23 +0100

    FIX changes on geocode unitary and bulk

--- api/v01/api_base.rb
+++ api/v01/api_base.rb
@@ -29,6 +29,34 @@ module Api
           ::GeocoderWrapper.access[api_key].except(:profile)
         )
       end
+
+      helpers do
+        params :geocode_unitary_params do |options|
+          requires :country, type: String, desc: 'Simple country name, ISO 3166-alpha-2 or ISO 3166-alpha-3.'
+          optional :housenumber, type: String
+          optional :street, type: String, allow_blank: false
+          optional :maybe_street, type: Array[String], desc: 'One undetermined entry of the array is the street, selects the good one for the geocoding process. Need to add an empty entry as alternative if you are unsure if there is a street in the list. Mutually exclusive field with street field.', documentation: { param_type: options[:type] || 'query'}
+          mutually_exclusive :street, :maybe_street
+          optional :postcode, type: String, allow_blank: false
+          optional :city, type: String, allow_blank: false
+          optional :state, type: String
+          optional :query, type: String, allow_blank: false, desc: 'Full text, free form, address search.'
+          at_least_one_of :query, :postcode, :city, :street
+          mutually_exclusive :query, :street
+          mutually_exclusive :query, :maybe_street
+          mutually_exclusive :query, :postcode
+          mutually_exclusive :query, :city
+          optional :type, type: String, desc: 'Queried result type filter. One of "house", "street", "locality", "city", "region", "country".'
+          optional :lat, type: Float, desc: 'Prioritize results around this latitude.'
+          optional :lng, type: Float, desc: 'Prioritize results around this longitude.'
+          optional :limit, type: Integer, desc: 'Max results numbers. (default and upper max 10)'
+        end
+
+        params :reverse_unitary_params do
+          requires :lat, type: Float, desc: 'Latitude.'
+          requires :lng, type: Float, desc: 'Longitude.'
+        end
+      end
     end
   end
 end
