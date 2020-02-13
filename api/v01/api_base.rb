963263758aa5259a0f3f06a074d9ed20588f4d22
yann-g <yann.gregoire@mapotempo.com>
2020-02-13 15:48:50 +0100

    FIX move helpers to APIBase

--- api/v01/api_base.rb
+++ api/v01/api_base.rb
@@ -56,6 +56,77 @@ module Api
           requires :lat, type: Float, desc: 'Latitude.'
           requires :lng, type: Float, desc: 'Longitude.'
         end
+
+        def redis_count
+          GeocoderWrapper.config[:redis_count]
+        end
+
+        def count_time
+          @count_time ||= Time.now.utc
+        end
+
+        def count_base_key(operation, period = :daily)
+          count_date = if period == :daily
+            count_time.to_s[0..9]
+          elsif period == :monthly
+            count_time.to_s[0..6]
+          elsif period == :yearly
+            count_time.to_s[0..3]
+          end
+          [
+            [:geocoder, operation, count_date].compact,
+            [:key, params[:api_key]]
+          ].map{ |a| a.join(':') }.join('_')
+        end
+
+        def count_key(operation)
+          @count_key ||= count_base_key(operation) + '_' + [
+            [:ip, (env['action_dispatch.remote_ip'] || request.ip).to_s],
+            [:asset, params[:asset]]
+          ].map{ |a| a.join(':') }.join('_')
+        end
+
+        def count(operation, raise_if_exceed = true, request_size = 1)
+          return unless redis_count
+
+          @count_val = redis_count.hgetall(count_key(operation)).symbolize_keys
+          if @count_val.empty?
+            @count_val = {hits: 0, transactions: 0}
+            redis_count.mapped_hmset @count_key, @count_val
+            redis_count.expire @count_key, 100.days
+          end
+          return unless raise_if_exceed
+
+          APIBase.profile(params[:api_key])[:quotas]&.each do |quota|
+            op = quota[:operation]
+            next unless op.nil? || op == operation
+
+            quota.slice(:daily, :monthly, :yearly).each do |k, v|
+              count = redis_count.get(count_base_key(op, k)).to_i
+              raise QuotaExceeded.new("Too many #{k} requests", limit: v, remaining: v - count, reset: k) if count + request_size > v
+            end
+          end
+        end
+
+        def count_incr(operation, options)
+          return unless redis_count
+
+          count operation, false unless @count_val
+          incr = {hits: @count_val[:hits].to_i + 1}
+          incr[:transactions] = @count_val[:transactions].to_i + options[:transactions] if options[:transactions]
+          redis_count.mapped_hmset @count_key, incr
+          return unless options[:transactions]
+
+          APIBase.profile(params[:api_key])[:quotas]&.each do |quota|
+            op = quota[:operation]
+            next unless op.nil? || op == operation
+
+            quota.slice(:daily, :monthly, :yearly).each do |k, _v|
+              redis_count.incrby count_base_key(op, k), options[:transactions]
+              redis_count.expire count_base_key(op, k), 366.days
+            end
+          end
+        end
       end
     end
   end
