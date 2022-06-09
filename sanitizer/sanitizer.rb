cdd3d310a1bbdd74f44ef893aeb42e556bcaabf9
fab-girard <fab.girard@gmail.com>
2022-06-09 12:55:18 +0200

    Detect country to sanitize

--- sanitizer/sanitizer.rb
+++ sanitizer/sanitizer.rb
@@ -23,13 +23,14 @@ module Sanitizer
       @rules = Hash[Dir.glob(rules_path + '*.yaml').collect{ |rules_file|
         [rules_file.gsub(/.*\//, '').gsub(/.yaml$/, ''), load_rules(rules_file)]
       }]
-      @country_languages = load_country_languages(country_info)
+      @country_info = country_info
+      @country_languages = load_country_languages
       @rules_by_country = {}
     end
 
-    def sanitize(params)
+    def sanitize(params, country)
       [:query, :street, :maybe_street].each{ |field|
-        params[field] = matching_rules(params).reduce(params[field]) { |text, rule|
+        params[field] = matching_rules(country).reduce(params[field]) { |text, rule|
           text.gsub(rule, '')
         } if params[field]
       }
@@ -50,9 +51,21 @@ module Sanitizer
       }
     end
 
-    def matching_rules(params)
-      country = params[:country]
-      @rules_by_country[country] ||= matching_rules_by_country(country)
+    def matching_rules(country)
+      country_code = matching_country(country)
+      @rules_by_country[country_code] ||= matching_rules_by_country(country_code)
+    end
+
+    def matching_country(country)
+      load_country_info.find{ |row|
+        row[0].downcase.to_sym == country || row[1].downcase.to_sym == country || row[4].downcase.to_sym == country
+      }.try(:[], 0)
+    end
+
+    def load_country_info
+      @load_country_info ||= CSV.read(@country_info, col_sep: "\t").select{ |row|
+        row[0][0] != '#' && row[15]
+      }
     end
 
     def matching_rules_by_country(country)
@@ -60,11 +73,9 @@ module Sanitizer
       keys.flat_map{ |key| @rules[key] }.compact
     end
 
-    def load_country_languages(country_info)
-      Hash[CSV.read(country_info, col_sep: "\t").select{ |row|
-        row[0][0] != '#' && row[15]
-      }.collect{ |row|
-        [row[0], row[15].split(',')]
+    def load_country_languages
+      Hash[load_country_info.collect{ |row|
+        [row[0], row[15].split(',').map{ |locale| locale.gsub(/\-.+/, '') }]
       }]
     end
   end
