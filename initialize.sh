36b40dbf79d189a337e3530f1f43817ecf569169
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-07-13 15:49:38 +0200

    Align BANO data type to BAN

--- initialize.sh
+++ initialize.sh
@@ -5,7 +5,7 @@ mkdir -p addresses
 # shellcheck disable=SC1091
 source ./docker/builder/get_bano.sh
 
-docker-compose -p "${PROJECT}" run --rm --entrypoint /bin/bash addok -c "zcat ./addresses/BAN_odbl.sjson.gz | jq -c 'del(.housenumbers[]?.id)' | addok batch"
+docker-compose -p "${PROJECT}" run --rm --entrypoint /bin/bash addok -c "zcat ./addresses/BAN_odbl.sjson.gz | jq -c 'def mapping: {\"city\":\"municipality\",\"town\":\"municipality\",\"village\":\"municipality\",\"place\":\"locality\",\"street\":\"street\"}; . + {type: mapping[.type]}' | jq -c 'del(.housenumbers[]?.id)' | addok batch"
 
 # Patch BAN
 docker-compose -p "${PROJECT}" run --rm --entrypoint /bin/bash addok -c "ls ./addresses/*.json | xargs cat | addok batch"
