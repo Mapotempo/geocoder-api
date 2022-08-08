fd728472a0fc55d3732e1531c30132fa279c9fbd
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-08-08 15:18:50 +0200

    New BAN support update

--- docker/builder/get_bano.sh
+++ docker/builder/get_bano.sh
@@ -8,8 +8,8 @@ echo "Your variables are : PROJECT : $PROJECT, DEPARTMENT: $DEP"
 
 # Download and load BAN
 if [ "${DEP}" == full ]; then
-  BAN="http://bano.openstreetmap.fr/data/full.sjson.gz"
+  BAN="https://adresse.data.gouv.fr/data/ban/adresses/latest/addok/adresses-addok-france.ndjson.gz"
 else
-  BAN="http://bano.openstreetmap.fr/data/bano-${DEP}.json.gz"
+  BAN="https://adresse.data.gouv.fr/data/ban/adresses/latest/addok/adresses-addok-${DEP}.ndjson.gz"
 fi
 wget "$BAN" -O "./addresses/BAN_odbl.sjson.gz"
